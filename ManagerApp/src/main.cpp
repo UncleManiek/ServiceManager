f303390b64963f26431ba79fef72302acae84a83
UncleManiek <mpi.serwisy@gmail.com>
2023-12-06 00:28:49 +0100

    Added manager app config installation. Added content co config json files. Manager app - sample code for reading config test.

--- ManagerApp/src/main.cpp
+++ ManagerApp/src/main.cpp
@@ -1,15 +1,36 @@
-#include <iostream>
+#include <fstream>
+#include <string>
+
 #include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 
+//temporary set path
+const std::string config_path = "/etc/ServiceManagerConfig.json";
 
 using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "Starting service manager" << std::endl;
-    spdlog::info("Test spdlog");
+    spdlog::info("Service Manager App started");
 
-    json empty_array_explicit = json::array();
+    /*Reading app configuration*/
+    std::ifstream config_file(config_path);
+
+    if(!config_file.is_open()){
+        spdlog::error("Cannot open configuration file:" + config_path +". Make sure file exists...");
+        return 1;
+    }
+
+    nlohmann::json config_data;
+    config_file >> config_data;
+
+    if(config_data.is_null()){
+        spdlog::error("Error during reading configuration file: configuration is empty");
+        return 2;
+    }
+
+    /*Print config data for checking purpose*/
+    spdlog::info("{}", config_data.dump(4));
+
     return 0;
 }
