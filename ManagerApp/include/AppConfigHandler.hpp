0dadf82066dcd26dab969c0839245bb7a47d0dce
UncleManiek <mpi.serwisy@gmail.com>
2023-12-26 19:26:41 +0100

    Implementation skeleton for AppConfigHandler

--- ManagerApp/include/AppConfigHandler.hpp
+++ ManagerApp/include/AppConfigHandler.hpp
@@ -2,10 +2,41 @@
 #include <nlohmann/json.hpp>
 
 #include <utility>
+#include <vector>
+#include <string_view>
+
 #include <ConfigHandler.hpp>
 
 class AppConfigHandler : public ConfigHandler
 {
 public:
+    /**
+     * @brief Constructor
+     */
     AppConfigHandler(const std::string& config_file_path);
+
+    /**
+     * @brief Overwritten implementation of validation json data. Data is validated right after reading.
+    */
+    ConfigResult validateConfigData();
+
+    /**
+     * @brief Function for getting all apps names from json data
+     * @return string_view vector of apps name
+    */
+    const std::vector<std::string_view> getAllAppsName();
+
+    /**
+     * @brief Function for getting particular app path.
+     * @param app_name - reference for application name - if invalid function returns empty string
+     * @return string_view with application path - if argument is invalid - it returns empty string.
+    */
+    const std::string_view getAppPath(const std::string_view &app_name);
+
+    /**
+     * @brief Function for getting all params for specific app
+     * @param app_name reference to string_view object with application name in json data
+     * @return function returns srtring_view vector wit all app parameter
+    */
+    const std::vector<std::string_view> getAppParams(const std::string_view &app_name);
 };
--- ManagerApp/src/AppConfigHandler.cpp
+++ ManagerApp/src/AppConfigHandler.cpp
@@ -6,5 +6,12 @@ AppConfigHandler::AppConfigHandler(const std::string &config_file_path) : Config
     spdlog::debug("Created application config handler instance");
 }
 
+ConfigResult AppConfigHandler::validateConfigData()
+{
+    // following steps should be run for perform validation
 
-
+    //1. validate if apps array exists
+    //2. validate if all elements in apps array contains following obligatory fields: app_name, app_path
+    //3. validate if in particular app params fields are in proper order and params nums are fine
+    return ConfigResult::SUCCESS;
+}
